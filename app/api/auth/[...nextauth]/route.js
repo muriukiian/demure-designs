bd29c64d2f82eb4adf683519f9a801ee07694484
muriukiian <muriukiian47@gmail.com>
2024-09-17 21:11:27 +0300

    initial commit

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,50 +1,50 @@
 import connectDB from "@/config/db";
 import User from "@/schema/page";
-import bcrypt from 'bcryptjs'
+import bcrypt from 'bcryptjs';
 import NextAuth from "next-auth/next";
 import CredentialsProvider from "next-auth/providers/credentials";
 
-
 export const authOptions = {
-    providers:[
+    providers: [
         CredentialsProvider({
-            id:"credentials",
-            name:"credentials",
-            credentials:{
-                email:{
-                    label:"Email",
-                    type: "text"
-                },
-                password:{
-                    label:"Password",
-                    type:"password"
-                }
+            id: "credentials",
+            name: "credentials",
+            credentials: {
+                email: { label: "Email", type: "text" },
+                password: { label: "Password", type: "password" }
             },
-            
-            async authorize (credentials){
+            async authorize(credentials) {
                 await connectDB();
                 try {
-                    const user = await User.findOne({email: credentials.email});
-                    if(user){
+                    const user = await User.findOne({ email: credentials.email });
+                    if (user) {
                         const isPasswordCorrect = await bcrypt.compare(credentials.password, user.password);
-                        if(isPasswordCorrect) {
+                        if (isPasswordCorrect) {
                             return user;
+                        } else {
+                            console.error('Invalid password');
+                            throw new Error('Invalid credentials');
                         }
+                    } else {
+                        console.error('User not found');
+                        throw new Error('User not found');
                     }
                 } catch (error) {
-                    throw new Error(error)
+                    console.error('Authorization error:', error);
+                    throw new Error('Authorization error');
                 }
             }
         })
     ],
-    callbacks:{
-        async signIn({user, account}){
-            if(account?.provider == 'credentials'){
-                return true
+    callbacks: {
+        async signIn({ user, account }) {
+            if (account?.provider == 'credentials') {
+                return true;
             }
+            return false;
         }
     }
-}
+};
 
 export const handler = NextAuth(authOptions);
-export {handler as GET, handler as POST};
+export { handler as GET, handler as POST };
